405cd00e30be2b6b815859f8e304936a8e1595a7
Francisco <javiermartincuenca@gmail.com>
2021-11-27 07:51:17 +0100

    lesson 4

---
+++ pages/04-grid-lines-by-name.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <header>Header</header>
+    <aside>Sidebar</aside>
+    <article>Content</article>
+    <footer>Footer</footer>
+  </main>
+</template>
+
+<style>
+main {
+  display: grid;
+  height: 100vh;
+  grid-gap: 10px;
+  /* grid-template-columns: 1fr 2fr; */
+
+  grid-template-columns:
+    [sidebar-start] 1fr
+    [content-start] 2fr;
+
+  grid-template-rows:
+    [header-start] 1fr
+    [content-start] 2fr
+    [footer-start] 1fr;
+}
+
+main > * {
+  font-family: sans-serif;
+  background-color: goldenrod;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header {
+  grid-column: sidebar-start / span 2;
+}
+
+aside {
+  grid-row: content-start / span 2;
+}
+
+footer {
+  grid-column: content-start;
+}
+</style>
