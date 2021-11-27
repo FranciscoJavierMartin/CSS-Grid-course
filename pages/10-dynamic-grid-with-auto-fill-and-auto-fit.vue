5381aba4b14de1782fad921d166475d5ba395a43
Francisco <javiermartincuenca@gmail.com>
2021-11-27 14:32:49 +0100

    lesson 10

---
+++ pages/10-dynamic-grid-with-auto-fill-and-auto-fit.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="auto-fill">
+    <div>Column 1</div>
+    <div>Column 2</div>
+    <div>Column 3</div>
+    <div>Column 4</div>
+  </section>
+  <section class="auto-fit">
+    <div>Column 1</div>
+    <div>Column 2</div>
+    <div>Column 3</div>
+    <div>Column 4</div>
+  </section>
+</template>
+
+<style>
+section {
+  display: grid;
+  grid-gap: 10px;
+  font-family: sans-serif;
+  margin: 20px 0;
+}
+
+section div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  text-align: center;
+}
+
+.auto-fill div {
+  background-color: goldenrod;
+}
+
+.auto-fit div {
+  background-color: purple;
+  color: white;
+}
+
+.auto-fill {
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+}
+
+.auto-fit {
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+}
+</style>
