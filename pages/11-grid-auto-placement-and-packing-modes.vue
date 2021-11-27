c17ad89430e3bc3f878953b7f175151b5434ef07
Francisco <javiermartincuenca@gmail.com>
2021-11-27 22:38:45 +0100

    lesson 11

---
+++ pages/11-grid-auto-placement-and-packing-modes.vue
@@ -0,0 +1,107 @@
+<template>
+  <main>
+    <section>Element 001</section>
+    <section>Element 002</section>
+    <section>Element 003</section>
+    <section>Element 004</section>
+    <section>Element 005</section>
+    <section>Element 006</section>
+    <section>Element 007</section>
+    <section>Element 008</section>
+    <section>Element 009</section>
+    <section>Element 010</section>
+    <section>Element 011</section>
+    <section>Element 012</section>
+    <section>Element 013</section>
+    <section>Element 014</section>
+    <section>Element 015</section>
+    <section>Element 001</section>
+    <section>Element 002</section>
+    <section>Element 003</section>
+    <section>Element 004</section>
+    <section>Element 005</section>
+    <section>Element 006</section>
+    <section>Element 007</section>
+    <section>Element 008</section>
+    <section>Element 009</section>
+    <section>Element 010</section>
+    <section>Element 011</section>
+    <section>Element 012</section>
+    <section>Element 013</section>
+    <section>Element 014</section>
+    <section>Element 015</section>
+    <section>Element 001</section>
+    <section>Element 002</section>
+    <section>Element 003</section>
+    <section>Element 004</section>
+    <section>Element 005</section>
+    <section>Element 006</section>
+    <section>Element 007</section>
+    <section>Element 008</section>
+    <section>Element 009</section>
+    <section>Element 010</section>
+    <section>Element 011</section>
+    <section>Element 012</section>
+    <section>Element 013</section>
+    <section>Element 014</section>
+    <section>Element 015</section>
+    <section>Element 001</section>
+    <section>Element 002</section>
+    <section>Element 003</section>
+    <section>Element 004</section>
+    <section>Element 005</section>
+    <section>Element 006</section>
+    <section>Element 007</section>
+    <section>Element 008</section>
+    <section>Element 009</section>
+    <section>Element 010</section>
+    <section>Element 011</section>
+    <section>Element 012</section>
+    <section>Element 013</section>
+    <section>Element 014</section>
+    <section>Element 015</section>
+    <section>Element 001</section>
+    <section>Element 002</section>
+    <section>Element 003</section>
+    <section>Element 004</section>
+    <section>Element 005</section>
+    <section>Element 006</section>
+    <section>Element 007</section>
+    <section>Element 008</section>
+    <section>Element 009</section>
+    <section>Element 010</section>
+    <section>Element 011</section>
+    <section>Element 012</section>
+    <section>Element 013</section>
+    <section>Element 014</section>
+    <section>Element 015</section>
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background-color: white;
+}
+
+main {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(150px, 1fr));
+  grid-auto-rows: minmax(150px, 1fr);
+  grid-auto-columns: minmax(150px, 1fr);
+  grid-auto-flow: column dense;
+}
+
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: goldenrod;
+  border: 1px solid black;
+}
+</style>
