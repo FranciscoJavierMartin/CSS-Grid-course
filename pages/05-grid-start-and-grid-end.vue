7ab2a0952ae4197b926c93b4cd3ff4edc6726c30
Francisco <javiermartincuenca@gmail.com>
2021-11-27 08:05:41 +0100

    lesson 5

---
+++ pages/05-grid-start-and-grid-end.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <header>Header</header>
+    <aside>Sidebar</aside>
+    <article>Content</article>
+    <footer>Footer</footer>
+  </main>
+</template>
+
+<style>
+main {
+  display: grid;
+  height: 100vh;
+  grid-gap: 10px;
+  /* grid-template-columns: 1fr 2fr; */
+
+  grid-template-columns:
+    [sidebar-start] 1fr
+    [sidebar-end content-start] 2fr
+    [content-end];
+
+  grid-template-rows:
+    [header-start] 1fr
+    [header-end content-start] 2fr
+    [footer-start] 1fr
+    [footer-end];
+}
+
+main > * {
+  font-family: sans-serif;
+  background-color: goldenrod;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header {
+  grid-column: sidebar-start / content-end;
+}
+
+aside {
+  grid-row: header-end / footer-end;
+}
+
+footer {
+  grid-column: content-start / content-end;
+}
+</style>
