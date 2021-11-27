c0e82b75db6fe216975b9d97ccdc12c4c8c1b520
Francisco <javiermartincuenca@gmail.com>
2021-11-27 22:49:54 +0100

    lesson 12

---
+++ pages/12-implicit-and-explicit-grid.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>10</div>
+    <div>11</div>
+    <div>12</div>
+    <div>13</div>
+    <div>14</div>
+    <div>15</div>
+  </main>
+</template>
+
+<style>
+main {
+  display: grid;
+  height: 100vh;
+  grid-gap: 10px;
+  grid-template-columns: 100px 50px;
+  grid-auto-columns: 300px;
+  grid-auto-flow: column;
+}
+
+main div {
+  background-color: goldenrod;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: sans-serif;
+}
+
+main div:first-child,
+main div:nth-child(2) {
+  background-color: purple;
+  color: white;
+}
+</style>
