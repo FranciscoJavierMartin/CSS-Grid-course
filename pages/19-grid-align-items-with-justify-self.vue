2dadda58b2845fd79ff14699e61608cca59ef8a9
Francisco <javiermartincuenca@gmail.com>
2021-11-30 21:36:53 +0100

    lesson 19

---
+++ pages/19-grid-align-items-with-justify-self.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>10</div>
+    <div>11</div>
+    <div>12</div>
+  </main>
+</template>
+
+<style>
+main {
+  display: grid;
+  grid-gap: 10px;
+  font-family: sans-serif;
+  grid-template: repeat(4, 50px) / repeat(3, 1fr);
+  /* Row axis */
+  justify-items: stretch;
+  /* Column axis */
+  align-items: stretch;
+}
+
+main div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: goldenrod;
+  justify-self: start;
+}
+
+main div:nth-child(5) {
+  background-color: purple;
+  color: white;
+  justify-self: end;
+}
+</style>
